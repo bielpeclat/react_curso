b6a9be057e42f08561d26f451cdfc173923f501d
bielpeclat <bielpeclat1@gmail.com>
2026-04-25 15:46:24 +0100

    Fim da aula 3

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 
 function App() {
+
+  const name = 'Gabriel'
+  const newName = name.toUpperCase();
+
+  const sum = (a,b) => a + b;
+
+  const url = 'https://placehold.co/250';
+  
   return (
     <div className="App">
-      <h1>Olá React!</h1>
-      <p>Meu primeiro App</p>
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {newName}</p>
+      <p>Soma: {sum(3,6)}</p>
+      <img alt="Minha imagem" src={url}></img>
     </div>
   );
 }
